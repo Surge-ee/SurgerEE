2c6ba55b8971af2ba8ea549766616646b49702b7
Daniel Poulin <dan.poulin@digitalsurgeons.com>
2012-05-02 10:38:17 -0400

    Initial commit

---
+++ pi.dsutils.php
@@ -0,0 +1,106 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		ExpressionEngine Dev Team
+ * @copyright	Copyright (c) 2003 - 2011, EllisLab, Inc.
+ * @license		http://expressionengine.com/user_guide/license.html
+ * @link		http://expressionengine.com
+ * @since		Version 2.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * DSUtils Plugin
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Addons
+ * @category	Plugin
+ * @author		Daniel Poulin
+ * @link
+ */
+
+$plugin_info = array(
+	'pi_name'		=> 'DSUtils',
+	'pi_version'	=> '1.0',
+	'pi_author'		=> 'Daniel Poulin',
+	'pi_author_url'	=> '',
+	'pi_description'=> 'Various commonly needed items that make us want to use php in templates.',
+	'pi_usage'		=> Dsutils::usage()
+);
+
+
+class Dsutils {
+
+	public $return_data;
+
+	/** Constructor */
+	public function __construct() {
+		$this->EE =& get_instance();
+	}
+
+	/** Applies the modulo operator to a numerator and denominator.
+	 *
+	 * Useful for outputting stuff every third, fourth, etc entry in
+	 * a loop. */
+	function modulo() {
+		$numerator = $this->EE->TMPL->fetch_param('numerator');
+		$denomenator = $this->EE->TMPL->fetch_param('denomenator');
+		$this->return_data = $numerator % $denomenator;
+	}
+
+	/** Checks if an integer is halfway rounded up through the total.
+	 *
+	 * Useful for determining if you are halfway through a loop. */
+	function is_halfway() {
+		$count = $this->EE->TMPL->fetch_param('count');
+		$total = $this->EE->TMPL->fetch_param('total');
+		$this->return_data = ($count == ceil($total / 2));
+	}
+
+	/** Performs a regex replace on a string */
+	function replace() {
+		$string = $this->EE->TMPL->fetch_param('string', '');
+		$regex   = $this->EE->TMPL->fetch_param('regex', '');
+		$replace  = $this->EE->TMPL->fetch_param('replace', '');
+
+		//Output transformed string
+		$this->return_data = preg_replace("/$regex/", $replace, $string);
+	}
+
+	/** Searches for a regex in a string */
+	function match() {
+		$string = $this->EE->TMPL->fetch_param('string', '');
+		$regex   = $this->EE->TMPL->fetch_param('regex', '');
+
+		//Output transformed string
+		$this->return_data = preg_match("/$regex/", $string);
+	}
+
+	// -- Plugin Usage -- //
+	public static function usage() {
+		ob_start();
+?>
+	Modulo:
+		{exp:dsutils:modulo numerator="4" denominator="3"}
+	Output:
+		1
+
+	Is Halfway:
+		{exp:dsutils:is_halfway count="5" total="9"}
+	Output:
+		true
+<?php
+		$buffer = ob_get_contents();
+		ob_end_clean();
+		return $buffer;
+	}
+}
+
+
+/* End of file pi.dsutils.php */
+/* Location: /system/expressionengine/third_party/dsutils/pi.dsutils.php */
