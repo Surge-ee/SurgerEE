6e0a7581d3bb0b56ddc5eec5807c2a9d9e7425a2
Daniel Poulin <dan.poulin@digitalsurgeons.com>
2012-05-24 15:31:46 -0400

    Bump version number

--- pi.surgeree.php
+++ pi.surgeree.php
@@ -31,7 +31,7 @@
 
 $plugin_info = array(
 	'pi_name'		=> 'SurgerEE',
-	'pi_version'	=> '1.4.1',
+	'pi_version'	=> '1.4.2',
 	'pi_author'		=> 'Daniel Poulin',
 	'pi_author_url'	=> 'http://github.com/dsurgeons/SurgerEE',
 	'pi_description'=> 'Various commonly needed items that make us want to use php in templates.',
