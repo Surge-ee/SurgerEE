7dc782e81c64aa45d2aebbd4334c20461664a416
Daniel Poulin <dan.poulin@digitalsurgeons.com>
2012-05-24 15:34:52 -0400

    Reattribute author to organization.
    Keep individual authors ins ource comments.

--- pi.surgeree.php
+++ pi.surgeree.php
@@ -32,7 +32,7 @@
 $plugin_info = array(
 	'pi_name'		=> 'SurgerEE',
 	'pi_version'	=> '1.4.2',
-	'pi_author'		=> 'Daniel Poulin',
+	'pi_author'		=> 'Digital Surgeons',
 	'pi_author_url'	=> 'http://github.com/dsurgeons/SurgerEE',
 	'pi_description'=> 'Various commonly needed items that make us want to use php in templates.',
 	'pi_usage'		=> Surgeree::usage()
