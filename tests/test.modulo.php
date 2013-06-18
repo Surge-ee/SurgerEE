87331de376c14170c489c5f501f61c534de43f82
Daniel Poulin <dan.poulin@digitalsurgeons.com>
2013-06-18 17:03:26 -0400

    Complete modulo tests

--- tests/test.modulo.php
+++ tests/test.modulo.php
@@ -15,8 +15,41 @@ class Test_modulo extends Surgeree_unit_test_case {
 
 		$this->_setParam('numerator', 11);
 		$this->_setParam('denominator', 2);
+
 		$result = $this->_subject->modulo();
 		$this->assertEqual($result, 1);
+
+	}
+
+	public function test__returns_zero_with_non_number_args() {
+
+		// Numerator fed string
+		$this->_setParam('numerator', 'hey there');
+		$this->_setParam('denominator', 2);
+		$result = $this->_subject->modulo();
+		$this->assertEqual($result, 0);
+
+		// Denominator fed string
+		$this->_setParam('numerator', 11);
+		$this->_setParam('denominator', 'hey there');
+		$result = $this->_subject->modulo();
+		$this->assertEqual($result, 0);
+
+		// Both fed string
+		$this->_setParam('numerator', 'hey there');
+		$this->_setParam('denominator', 'hey there');
+		$result = $this->_subject->modulo();
+		$this->assertEqual($result, 0);
+
+	}
+
+	public function test__zero_denominator_rewritten_to_1() {
+
+		$this->_setParam('numerator', 11);
+		$this->_setParam('denominator', 0);
+
+		$result = $this->_subject->modulo();
+		$this->assertEqual($result, 0);
 
 	}
 
