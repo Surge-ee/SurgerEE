be40f2e00e5a67c8c37923519a3b239f68abf4c9
Daniel Poulin <dan.poulin@digitalsurgeons.com>
2013-06-18 17:03:00 -0400

    Add shortcut for setting tagdata to surgeree unit test case.

--- classes/surgeree_unit_test_case.php
+++ classes/surgeree_unit_test_case.php
@@ -16,7 +16,7 @@ class Surgeree_unit_test_case extends Testee_unit_test_case {
 	}
 
 	/**
-	 * This is a shortcut for setting the value of a parameter for a plugin tag.
+	 * This is a shortcut for mocking the value of a parameter for a plugin tag.
 	 *
 	 * @param  string $param The parameter name.
 	 * @param  string $value The value to set the parameter to.
@@ -25,6 +25,18 @@ class Surgeree_unit_test_case extends Testee_unit_test_case {
 	protected function _setParam($param, $value) {
 
 		$this->_subject->EE->TMPL->returns('fetch_param', $value, array($param, '*'));
+
+	}
+
+	/**
+	 * Shortcut for mocking what's between a tag pair.
+	 *
+	 * @param  string $data Whatever is supposed to be inside the tag pair.
+	 * @return void
+	 */
+	protected function _setTagdata($data) {
+
+		$this->_subject->EE->TMPL->tagdata = $data;
 
 	}
 
